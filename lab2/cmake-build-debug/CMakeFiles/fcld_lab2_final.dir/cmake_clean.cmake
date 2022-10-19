9ee21b58eb91488f4cd8d3dd97300187ac7a9da0
915-Pop-Calin <72080637+915-Pop-Calin@users.noreply.github.com>
2022-10-19 18:01:34 +0300

    lab1 + 2

---
+++ lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fcld_lab2_final.dir/SymbolTable.cpp.o"
+  "CMakeFiles/fcld_lab2_final.dir/main.cpp.o"
+  "fcld_lab2_final"
+  "fcld_lab2_final.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fcld_lab2_final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
