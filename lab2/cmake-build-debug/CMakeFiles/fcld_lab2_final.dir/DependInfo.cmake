f2fe2f3604eccc9f9a0a9fa0f680f0502a3d3b86
915-Pop-Calin <72080637+915-Pop-Calin@users.noreply.github.com>
2022-11-12 15:07:02 +0200

    Revert "Delete lab2/cmake-build-debug directory"
    This reverts commit 658ea875e824371e98e1c785ec6f6b9e76976b07.

---
+++ lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/popca/PycharmProjects/FLCD/lab2/data_structure/implementation/HashTable.cpp" "C:/Users/popca/PycharmProjects/FLCD/lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/data_structure/implementation/HashTable.cpp.obj"
+  "C:/Users/popca/PycharmProjects/FLCD/lab2/data_structure/implementation/PIF.cpp" "C:/Users/popca/PycharmProjects/FLCD/lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/data_structure/implementation/PIF.cpp.obj"
+  "C:/Users/popca/PycharmProjects/FLCD/lab2/data_structure/implementation/SymbolTable.cpp" "C:/Users/popca/PycharmProjects/FLCD/lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/data_structure/implementation/SymbolTable.cpp.obj"
+  "C:/Users/popca/PycharmProjects/FLCD/lab2/finite_automata/implementation/FiniteAutomata.cpp" "C:/Users/popca/PycharmProjects/FLCD/lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/finite_automata/implementation/FiniteAutomata.cpp.obj"
+  "C:/Users/popca/PycharmProjects/FLCD/lab2/main.cpp" "C:/Users/popca/PycharmProjects/FLCD/lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/main.cpp.obj"
+  "C:/Users/popca/PycharmProjects/FLCD/lab2/model/implementation/Token.cpp" "C:/Users/popca/PycharmProjects/FLCD/lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/model/implementation/Token.cpp.obj"
+  "C:/Users/popca/PycharmProjects/FLCD/lab2/scanner/implementation/Scanner.cpp" "C:/Users/popca/PycharmProjects/FLCD/lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/scanner/implementation/Scanner.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab2/cmake-build-debug/CMakeFiles/fcld_lab2_final.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fcld_lab2_final.dir/data_structure/implementation/HashTable.cpp.obj"
+  "CMakeFiles/fcld_lab2_final.dir/data_structure/implementation/PIF.cpp.obj"
+  "CMakeFiles/fcld_lab2_final.dir/data_structure/implementation/SymbolTable.cpp.obj"
+  "CMakeFiles/fcld_lab2_final.dir/finite_automata/implementation/FiniteAutomata.cpp.obj"
+  "CMakeFiles/fcld_lab2_final.dir/main.cpp.obj"
+  "CMakeFiles/fcld_lab2_final.dir/model/implementation/Token.cpp.obj"
+  "CMakeFiles/fcld_lab2_final.dir/scanner/implementation/Scanner.cpp.obj"
+  "fcld_lab2_final.exe"
+  "fcld_lab2_final.exe.manifest"
+  "fcld_lab2_final.pdb"
+  "libfcld_lab2_final.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fcld_lab2_final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
